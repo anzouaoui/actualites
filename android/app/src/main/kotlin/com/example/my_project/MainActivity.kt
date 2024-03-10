b3a8a51d16524e71d4fcacefb7cf544ca0933f5c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-10 15:04:18 +0000

    Mise en place de l'authentification avec Firebase

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.anoukzouaoui.actualites
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/backend/firebase/firebase_config.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+Future initFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyBFQYpxJMiXBKDZxh7aYlleOHjQ6fWcVOE",
+            authDomain: "actualites-83b0ec.firebaseapp.com",
+            projectId: "actualites-83b0ec",
+            storageBucket: "actualites-83b0ec.appspot.com",
+            messagingSenderId: "442239712064",
+            appId: "1:442239712064:web:b5b4bf01dedaad362518a5"));
+  } else {
+    await Firebase.initializeApp();
+  }
+}
